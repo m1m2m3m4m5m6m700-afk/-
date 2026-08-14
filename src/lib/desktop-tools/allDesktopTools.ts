8820e2e13c79bad70de7d039816672d707015540
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-15 02:39:32 +0300

    refactor(desktop-tools): remove duplicate aggregator

--- src/lib/desktop-tools/allDesktopTools.ts
+++
@@ -1,9 +0,0 @@
-import { desktopToolCatalog, type DesktopToolSpec } from "./catalog";
-import { desktopToolExtensions } from "./extensions";
-import { extendedDesktopToolSpecs } from "./extended";
-
-export const allDesktopTools: DesktopToolSpec[] = [
-  ...desktopToolCatalog,
-  ...desktopToolExtensions,
-  ...extendedDesktopToolSpecs,
-];
