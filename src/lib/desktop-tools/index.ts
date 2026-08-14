f2242c8dc0f16a9a74deb0fd5dd0a0af73cc940e
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-15 02:43:51 +0300

    Add supplemental desktop tool to unified catalog

--- src/lib/desktop-tools/index.ts
+++ src/lib/desktop-tools/index.ts
@@ -1,11 +1,13 @@
 import { desktopToolCatalog } from "./catalog";
 import { desktopToolExtensions } from "./extensions";
 import { extendedDesktopToolSpecs } from "./extended";
+import { supplementalDesktopTools } from "./supplemental";
 
 export const allDesktopTools = [
   ...desktopToolCatalog,
   ...desktopToolExtensions,
   ...extendedDesktopToolSpecs,
+  ...supplementalDesktopTools,
 ];
 
 export type AllDesktopTool = (typeof allDesktopTools)[number];
