2dc2449b4216cc4164d52a36292ee44b12052354
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-15 19:31:37 +0300

    fix: set site title to Flixo tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,7 +104,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "فليكسو تولس" },
+      { title: "Flixo tools" },
       {
         name: "description",
         content:
