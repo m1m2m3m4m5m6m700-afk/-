4c1aafa94b010c7f2dc61f18053a47a752a70800
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-18 00:47:01 +0300

    refactor(platform): add single tool platform entrypoint

---
+++ src/lib/tool-platform/index.ts
@@ -0,0 +1,5 @@
+export * from "./categories";
+export * from "./types";
+export * from "./promotion";
+export { getPublicToolRegistration, publicToolRegistrations } from "./publicDesktopTools";
+export { publicToolTestContracts } from "./testContracts";
