b7a2c106160419ea7c1cad71aef66faf9d5551fa
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-17 14:59:59 +0300

    Delete retired File Splitter route

--- src/routes/tools/file-splitter.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { createReadyToolHead, renderReadyToolPage } from "@/lib/tool-runtime/renderReadyToolPage";
-import { FileSplitterRuntime } from "@/lib/tool-runtime/tools/file-splitter";
-
-export const Route = createFileRoute("/tools/file-splitter")({
-  head: createReadyToolHead(FileSplitterRuntime),
-  component: renderReadyToolPage(FileSplitterRuntime),
-});
