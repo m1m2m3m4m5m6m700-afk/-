582aa1faea0100454104b3c7fea8ba8a506fd80e
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-21 22:46:35 +0300

    feat(tools): register all image tool routes

--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,24 @@ import { arImageCompressorRoute } from './routes/ar-image-compressor';
 import { enImageCompressorRoute } from './routes/en-image-compressor';
 import {
   enAiImageGeneratorRoute,
+  enBackgroundBlurRoute,
   enBackgroundRemoverRoute,
+  enCollageMakerRoute,
   enCropResizeRoute,
+  enExifCleanerRoute,
   enImageConverterRoute,
+  enImageEffectsRoute,
+  enImageToSvgRoute,
   enImageToTextRoute,
   enImageUpscalerRoute,
+  enMemeGeneratorRoute,
+  enMockupGeneratorRoute,
   enObjectRemoverRoute,
+  enPassportPhotoMakerRoute,
+  enPhotoColorizerRoute,
   enRasterToSvgRoute,
+  enSvgOptimizerRoute,
+  enWatermarkAdderRoute,
   enWatermarkRemoverRoute,
 } from './routes/image-tools';
 import { indexRoute } from './routes/index';
@@ -28,6 +39,17 @@ const routeTree = rootRoute.addChildren([
   enCropResizeRoute,
   enWatermarkRemoverRoute,
   enRasterToSvgRoute,
+  enPhotoColorizerRoute,
+  enBackgroundBlurRoute,
+  enPassportPhotoMakerRoute,
+  enWatermarkAdderRoute,
+  enMemeGeneratorRoute,
+  enCollageMakerRoute,
+  enImageEffectsRoute,
+  enExifCleanerRoute,
+  enSvgOptimizerRoute,
+  enMockupGeneratorRoute,
+  enImageToSvgRoute,
 ]);
 
 export const router = createRouter({ routeTree, defaultPreload: 'intent' });
