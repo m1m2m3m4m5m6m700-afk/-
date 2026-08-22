eb622671680b0538580fac7dbc0f41d13b87f4dd
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-22 12:51:35 +0300

    refactor(i18n): replace duplicated tool routes with explicit localized matrix

--- src/router.tsx
+++ src/router.tsx
@@ -1,61 +1,11 @@
 import { createRouter } from '@tanstack/react-router';
-import { arImageCompressorRoute } from './routes/ar-image-compressor';
-import { enImageCompressorRoute } from './routes/en-image-compressor';
-import {
-  enAiImageGeneratorRoute,
-  enBackgroundBlurRoute,
-  enBackgroundRemoverRoute,
-  enCollageMakerRoute,
-  enCropResizeRoute,
-  enExifCleanerRoute,
-  enImageConverterRoute,
-  enImageCropperRoute,
-  enImageEffectsRoute,
-  enImageOcrRoute,
-  enImageToSvgRoute,
-  enImageToTextRoute,
-  enImageUpscalerRoute,
-  enMemeGeneratorRoute,
-  enMockupGeneratorRoute,
-  enObjectRemoverRoute,
-  enPassportPhotoMakerRoute,
-  enPixRoute,
-  enRasterToSvgRoute,
-  enSeedRoute,
-  enSvgOptimizerRoute,
-  enWatermarkAdderRoute,
-  enWatermarkRemoverRoute,
-} from './routes/image-tools';
 import { indexRoute } from './routes/index';
+import { localizedToolRoutes } from './routes/localized-tool-routes';
 import { rootRoute } from './routes/__root';
 
 const routeTree = rootRoute.addChildren([
   indexRoute,
-  enImageCompressorRoute,
-  arImageCompressorRoute,
-  enBackgroundRemoverRoute,
-  enAiImageGeneratorRoute,
-  enImageUpscalerRoute,
-  enImageConverterRoute,
-  enImageToTextRoute,
-  enObjectRemoverRoute,
-  enCropResizeRoute,
-  enWatermarkRemoverRoute,
-  enRasterToSvgRoute,
-  enImageCropperRoute,
-  enImageOcrRoute,
-  enBackgroundBlurRoute,
-  enPassportPhotoMakerRoute,
-  enWatermarkAdderRoute,
-  enMemeGeneratorRoute,
-  enCollageMakerRoute,
-  enImageEffectsRoute,
-  enExifCleanerRoute,
-  enSvgOptimizerRoute,
-  enMockupGeneratorRoute,
-  enImageToSvgRoute,
-  enSeedRoute,
-  enPixRoute,
+  ...localizedToolRoutes,
 ]);
 
 export const router = createRouter({
